14d9f95c1ea448eb8dc5e090777660c86b90d7f1
yeonssu <dustn7197@naver.com>
2023-03-14 14:08:27 +0900

    Practice HTTPS

---
+++ src/main/java/com/codestates/Section4Week1Application.java
@@ -0,0 +1,13 @@
+package com.codestates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section4Week1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section4Week1Application.class, args);
+	}
+
+}
